2fd3d783a98466b4635cb29ed0a5efd1279d6455
nnunodev <nunotnunes@outlook.com>
2021-01-19 17:51:50 +0000

    added sidenav and navbar, and theme colors

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+// Materialize Components initialization 
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+// Api request Nomics
+
 let request = new XMLHttpRequest();
 
 request.open('GET', 'https://api.nomics.com/v1/currencies/ticker?key=dcf96660caa5113bc5b1cdfe3d303540&quote-currency=EUR')
 
-request.onload = function() {
-let data = JSON.parse(this.response);
-console.log(data[0].name + ' is valued :',parseInt(data[0].price),'€');
+request.onload = function () {
+    let data = JSON.parse(this.response);
+    let BitCoinName = data[0].name;
+    let BitCoinPrice = data[0].price;
+    let BitCoinVolume = data[0, 1].volume;
+
+
+
+
+    console.log(BitCoinName + ' is valued :', parseInt(BitCoinPrice), '€', parseInt(BitCoinVolume));
 }
 
 request.send();
